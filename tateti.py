38132dbcd976e866d59ebcd0afdafe55b75b2836
mussel <75176981+muss-el@users.noreply.github.com>
2021-07-06 22:16:20 -0300

    Creadas primeras funciones.

---
+++ tateti.py
@@ -0,0 +1,35 @@
+def mostrarTablero(posiciones):
+    tablero = ( f""" {posiciones[0]} | {posiciones[1]} | {posiciones[2]}
+-----------
+ {posiciones[3]} | {posiciones[4]} | {posiciones[5]}
+-----------
+ {posiciones[6]} | {posiciones[7]} | {posiciones[8]}""")
+    return tablero
+posiciones = list(i for i in range(0,9))
+
+#posiciones[6], posiciones [7], posiciones[8] = "X", "X", "X"
+#mostrarTablero(posiciones)
+
+def jugadasGanadoras(posiciones):
+    jugadaUno = [posiciones[0] == posiciones[1] == posiciones[2], posiciones[0]]
+    jugadaDos = [posiciones[0] == posiciones[3] == posiciones[6], posiciones[0]]
+    jugadaTres = [posiciones[0] == posiciones[4] == posiciones[8], posiciones[0]]
+    jugadaCuatro = [posiciones[1] == posiciones[4] == posiciones[7], posiciones[1]]
+    jugadaCinco = [posiciones[2] == posiciones[5] == posiciones[8], posiciones[2]]
+    jugadaSeis = [posiciones[2] == posiciones[4] == posiciones[6], posiciones[2]]
+    jugadaSiete = [posiciones[3] == posiciones[4] == posiciones[5], posiciones[3]]
+    jugadaOcho = [posiciones[6] == posiciones[7] == posiciones[8], posiciones[6]]
+    jugadas = (jugadaUno, jugadaDos, jugadaTres, jugadaCuatro, jugadaCinco, jugadaSeis, jugadaSiete, jugadaOcho)
+    for i in range(0,8):
+        if (jugadas[i])[0]:
+            return (jugadas[i])[1]
+        else:
+            continue
+        return False
+
+while True:
+    try:
+        print (mostrarTablero(posiciones), "\nIngresá una posición valida.")
+        posicion = int(input(">>> "))
+    except:
+        pass
